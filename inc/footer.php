c3a724706b8df159caec77bac1ef5940f8c74e1b
komal <143491178+komalgupta2403@users.noreply.github.com>
2024-08-11 15:01:31 +0530

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,34 @@
+<div class="container-fluid bg-white mt-5">
+    <div class="row">
+       <div class="col lg-4">
+         <h3 class="h-font fw-bold fs-3 mb-2">KG HOTEL</h3>
+         <p>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Vero, natus ea exercitationem mollitia magni iure neque repellendus placeat.
+             Reprehenderit quod saepe, 
+             libero delectus sapiente totam itaque quibusdam dignissimos necessitatibus omnis.
+         </p>
+       </div>
+       <div class="col-lg-4">
+         <h5 class="mb-3">links</h5>
+         <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+         <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+         <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+         <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+         <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a><br>
+       </div>
+       <div class="col-lg-4 p-4">
+         <h5 class=" mb-3">Designed By</h5>
+         <a href="#"class="d-inline-block text-dark text-decoration-none mb-2">
+           Komal gupta
+         </a><br>
+         <a href="#"class="d-inline-block text-dark text-decoration-none mb-2">
+          DSEU DWARKA CAMPUS
+         </a>
+       </div>
+    </div>
+   </div>
+   
+   <H6 class=" text-center bg-dark text-white p-3 m-0">DESIGNED AND DEVELOPED BY KOMAL GUPTA</H6>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
